1f8ad26953554ce41f2779f4908632c2e6f2e2ef
An <anaislcno@gmail.com>
2023-09-29 11:01:13 +0200

    update error gestion + add calculs for leap years

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const yearEl = document.querySelector("#year");
 const form = document.querySelector("#form");
 const inputs = document.querySelectorAll("input");
 
-// Fonctions d'utilité
+// Utilities functions
 
 const isRequired = (value) => (value.trim() === "" ? false : true);
 
@@ -41,34 +41,50 @@ const nbDaysPerMonth = (year) => {
   return daysPerMonth;
 };
 
-// Fonction error
+// Error function
 
 const showError = (input, message) => {
   input.classList.remove("success");
   input.classList.add("error");
 
+  const label = input.previousElementSibling;
+  label.classList.remove("success__label");
+  label.classList.add("error__label");
+
   const error = input.nextElementSibling;
   error.textContent = message;
 };
 
-// Fonction success
+// Success function
 
 const showSuccess = (input) => {
   input.classList.remove("error");
   input.classList.add("success");
 
+  const label = input.previousElementSibling;
+  label.classList.remove("error__label");
+  label.classList.add("success__label");
+
   const error = input.nextElementSibling;
   error.textContent = "";
 };
 
-// Fonctions de vérifications des champs
+// Function for day validation
 
 const checkDay = () => {
   let valid = false;
+
   const day = dayEl.value.trim();
+  const month = monthEl.value.trim();
+  const year = yearEl.value.trim();
+
+  const min = 1;
+  const max = nbDaysPerMonth(year)[month - 1];
 
   if (!isRequired(day)) {
     showError(dayEl, "This field is required");
+  } else if (!isBetween(day, min, max)) {
+    showError(dayEl, "Must be in a valid day");
   } else {
     showSuccess(dayEl);
     valid = true;
@@ -76,18 +92,27 @@ const checkDay = () => {
   return valid;
 };
 
+// Function for month validation
+
 const checkMonth = () => {
   let valid = false;
 
   const month = monthEl.value.trim();
+  const min = 1,
+    max = 12;
+
   if (!isRequired(month)) {
     showError(monthEl, "This field is required");
+  } else if (!isBetween(month, min, max)) {
+    showError(monthEl, "Must be in a valid month");
   } else {
     showSuccess(monthEl);
     valid = true;
   }
   return valid;
 };
+
+// Function for year validation
 
 const checkYear = () => {
   let valid = false;
@@ -99,7 +124,7 @@ const checkYear = () => {
   if (!isRequired(year)) {
     showError(yearEl, "This field is required");
   } else if (!isBetween(year, min, max)) {
-    invalidInputError("year", "Must be in the past");
+    showError(yearEl, "Must be in the past");
   } else {
     showSuccess(yearEl);
     valid = true;
@@ -107,7 +132,32 @@ const checkYear = () => {
   return valid;
 };
 
-// Fonction au submit
+// Calculate nb of leap years (for having the right amount of days)
+
+const numLeapYears = () => {
+  const startYear = yearEl.value.trim();
+  let yearRange = [];
+
+  // Array with the years in the range
+  for (let i = startYear; i <= currentYear; i++) {
+    yearRange.push(i);
+  }
+
+  let newArray = [];
+
+  // Check if leap year
+  yearRange.forEach((year) => {
+    if (isLeapYear(year)) {
+      newArray.push(year);
+    }
+  });
+
+  return newArray.length;
+};
+
+// Calculate age function
+
+// Submit function
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
